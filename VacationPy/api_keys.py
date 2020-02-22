d5d8ab44f868ebcbdfe9647556ee9052ffed6dca
Bustamante <Familia@Dianas-MacBook-Pro-3.local>
2020-02-21 17:30:10 -0800

    API-Challenge-Complete

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE!"
+
+# Google API Key
+g_key = "AIzaSyBsOL1xpvohoW9_r2ZtFZvHMtVO5cc3G2g"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "b4fccfc928850401ffef818b985523b1"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
